67b4a558c6b1c30c4757faf3996c5860f7f97317
adhi62103-maker <adhi62103@gmail.com>
2025-10-16 10:09:58 +0530

    appbar title added

--- lib/sample1.dart
+++ lib/sample1.dart
@@ -13,7 +13,6 @@ class Sample extends StatelessWidget {
         actions: [Icon(Icons.settings)],
         title: Text("Your Profile"),
         centerTitle: false,
-
       ),
       body: Container(
         width: double.infinity,
@@ -26,109 +25,125 @@ class Sample extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.deepPurpleAccent,
-                  child: Image.asset("assets/images.png"),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  "Vikas Assudani",
-                  style: TextStyle(
-                    color: Colors.deepPurpleAccent,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+              CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.deepPurpleAccent,
+              child: Image.asset("assets/images.png"),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Vikas Assudani",
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              "UI/UX Designer",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 25),
+            Align(alignment: Alignment.topLeft, child: Text("Your Email")),
+            SizedBox(
+              height: 55,
+              width: double.infinity,
+              child: Card(
+                color: Colors.grey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        "vicassudani909@gmail.com",
+                        style: TextStyle(color: Colors.black),
+                      ),SizedBox(width: 130,),
+                      Icon(Icons.email),
+                    ],
                   ),
                 ),
-                SizedBox(height: 5),
-                Text(
-                  "UI/UX Designer",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
-                SizedBox(height: 25),
-                Align(alignment: Alignment.topLeft, child: Text("Your Email")),
-                SizedBox(
-                  height: 55,
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.grey,
-                    child: Row(
-                      children: [
-                        Text(
-                          "vicassudani909@gmail.com",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        SizedBox(width: 140,),
-                        Icon(Icons.email)
-                      ],
+              ),
+            ),
+          SizedBox(height: 25),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text("Phone Number"),
+          ),
+          SizedBox(
+            height: 55,
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "+91 9876543210",
+                      style: TextStyle(color: Colors.black),
                     ),
-                  ),
-                ),
-                SizedBox(height: 25),
-                Align(alignment: Alignment.topLeft, child: Text("Phone Number")),
-                SizedBox(
-                  height: 55,
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.grey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            "+91 9876543210",
-                            style: TextStyle(color: Colors.black),
-                          ),
 
-                          SizedBox(width: 200,)
-,                          Icon(Icons.phone)
-                        ],
-                      ),
-                    ),
-                  ),
+                    SizedBox(width: 205),
+                    Icon(Icons.phone),
+                  ],
                 ),
-                SizedBox(height: 25),
-                Align(alignment: Alignment.topLeft, child: Text("Website")),
-                SizedBox(
-                  height: 55,
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.grey,
-                    child:  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "www.vicassudani.com",
-                            style: TextStyle(color: Colors.black),
-                          ),Icon(Icons.web)
-                        ],
-                      ),
-                    ),
-                  ),
-                SizedBox(height: 25),
-                Align(alignment: Alignment.topLeft, child: Text("Password")),
-                SizedBox(
-                  height: 55,
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.grey,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("*********", style: TextStyle(color: Colors.black)),
-                        Icon(Icons.visibility),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
+          SizedBox(height: 25),
+          Align(alignment: Alignment.topLeft, child: Text("Website")),
+          SizedBox(
+            height: 55,
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "www.vicassudani.com",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(Icons.web),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 25),
+          Align(alignment: Alignment.topLeft, child: Text("Password")),
+          SizedBox(
+            height: 55,
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "*********",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    Icon(Icons.visibility),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ],
         ),
       ),
+    ),)
+    ,
     );
   }
 }
